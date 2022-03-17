34dca0f995d8f6be922053a6803610d5889381a5
Andrew Furze <furze.andrew@gmail.com>
2022-03-16 22:49:59 -0600

    Handle double letters in the guess with only one instance in answer

--- wordle.py
+++ wordle.py
@@ -24,27 +24,41 @@ def check_guess(guess, answer):
 
     # Check each letter in the guess and response with 'B', 'Y', 'G'
     for x in range(5):
-        letter = guess[x]
+        try:
+            letter = guess[x]
 
-        # Check if letter has already occurred in guess
-        # If so, the answer must contain an additional of that letter as well
-        # else 'B' should be returned
-        if guess[:x+1].count(letter) > 1:
-            # Always check if this is the correct position first
-            if letter == answer[x]:
-                response += 'G'
-            elif answer.count(letter) > 1:
-                response += 'Y'
+            # Check if letter has already occurred in guess
+            # If so, the answer must contain an additional of that letter as well
+            # else 'B' should be returned
+            if guess.count(letter) > 1:
+                # Always check if this is the correct position first
+                if letter == answer[x]:
+                    response += 'G'
+                # If a future instance of the letter is in the right place, and
+                # the answer does not have two of this letter, return 'B'
+                elif (x < 5
+                      and guess[x:].index(letter) == answer[x:].index(letter)
+                      and answer.count(letter) == 1):
+                    response += 'B'
+                elif guess[:x].count(letter) == 0:
+                    response += 'Y'
+                elif answer.count(letter) > 1:
+                    response += 'Y'
+                else:
+                    response += 'B'
+
+            # If there is only one of this letter so far, apply normal rules
+            elif letter in answer:
+                if letter == answer[x]:
+                    response += 'G'
+                else:
+                    response += 'Y'
             else:
                 response += 'B'
-        # If there is only one of this letter so far, apply normal rules
-        elif letter in answer:
-            if letter == answer[x]:
-                response += 'G'
-            else:
-                response += 'Y'
-        else:
+        except ValueError:
             response += 'B'
+
+        #print(response)
 
     return response
 
