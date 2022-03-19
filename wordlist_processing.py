866c47cd550c69a8c2ff596fe87362f78b851282
Andrew Furze <furze.andrew@gmail.com>
2022-03-19 12:50:44 -0600

    Calculate percentage values for word frequency

--- wordlist_processing.py
+++ wordlist_processing.py
@@ -15,7 +15,17 @@ def main():
         if len(key) == 5 and val != 0:
             five_letter_words[key] = val
 
-    print(five_letter_words)
+    # Calculate frequency percentage
+    total = 0
+    for key, val in five_letter_words.items():
+        total += val
+
+    five_letter_words_pct = {}
+    for key, val in five_letter_words.items():
+        pct = val / total
+        five_letter_words_pct[key] = pct
+
+    print(five_letter_words_pct)
 
 
 if __name__ == '__main__':
